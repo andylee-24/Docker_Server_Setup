a59a846b31e422c3746e9c6ea2cf7ab9cf5f191a
TaeHan Lee <45648726+alanskye@users.noreply.github.com>
2023-04-08 15:45:19 +0900

    Update start_single_server.sh

--- start_single_server.sh
+++ start_single_server.sh
@@ -1,1 +1,11 @@
-docker run -d --storage-opt size=100G --cpus=".8" --gpus all --memory=48g -v /mnt/data:/mnt/data -v /mnt/shared:/mnt/shared --name $1 eclgpu4_pytorch
+docker run \
+  -d \
+  --storage-opt size=100G \
+  --cpus=".8" \
+  --gpus all \
+  --memory=48g \
+  -v /mnt/data:/mnt/data \
+  -v /mnt/shared:/mnt/shared \
+  --name $1 \
+  -p $2:22 \
+  eclgpu4_pytorch
