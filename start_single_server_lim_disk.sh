8507b4b4b14650026c45d6a6dbb07af10dbde8e8
alanskye <albatross.cse@gmail.com>
2024-04-02 14:32:55 +0900

    update

--- start_single_server_lim_disk.sh
+++ start_single_server_lim_disk.sh
@@ -11,12 +11,16 @@ docker run \
   --storage-opt size=512g \
   --cpus="32" \
   --ipc host \
-  --gpus '"device=0,1"' \
+  --gpus all \
   --memory=128g \
   -v /mnt:/mnt \
   --name $1 \
   -p $2:22 \
-  tfgpu:1.0
+  # --device /dev/kvm \
+  # --security-opt seccomp=unconfined \
+  # --cap-add SYS_ADMIN \
+  pytorch2.2.0:latest
 
 # -v [host_path]:[mounting_point_in_the_container]
 # --gpus all / '"device=1"'
+# enable kvm
