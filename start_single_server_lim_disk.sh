7101fef4f9c62f4fc50b839f7ae79b50b1736490
andylee-24 <albatross.cse@gmail.com>
2024-05-28 02:52:45 +0000

    update

--- start_single_server_lim_disk.sh
+++ start_single_server_lim_disk.sh
@@ -1,26 +1,56 @@
+#!/bin/bash
 
-#!/bin/sh
-if [ "$#" -ne 2 ]; then
-	echo "Usage: $0 [name] [port]"
-	exit 1
-fi
+
+# Display available Docker images
+echo "Available Docker images are below,"
+docker images
+
+# Prompt user to select an image
+echo -n "Enter the name:tag of the Docker image: "
+read image_name
+
+# Prompt user for container name
+echo -n "Container Name : "
+read container_name
+
+# Prompt user for port number
+echo -n "Port : "
+read port_number
+
+# Prompt user for CPU quota
+max_cpus=$(nproc)
+echo -n "CPUs quota	(up to $max_cpus) : "
+read cpus_quota
+
+# Prompt user for memory quota
+echo -n "Memory quota (GB) : "
+read memory_quota
+
+
+# Prompt user for storage qutoa
+echo -n "Storage quota (GB) : "
+read storage_quota
+
 
 docker run \
   -itd \
   --restart unless-stopped \
-  --storage-opt size=512g \
-  --cpus="32" \
-  --ipc host \
+  --storage-opt size="${storage_quota}g" \
+  --cpus=$cpus_quota \
+  --memory="${memory_quota}g" \
   --gpus all \
-  --memory=128g \
-  -v /mnt:/mnt \
-  --name $1 \
-  -p $2:22 \
-  # --device /dev/kvm \
-  # --security-opt seccomp=unconfined \
-  # --cap-add SYS_ADMIN \
-  pytorch2.2.0:latest
+  --name $container_name \
+  -p $port_number:22 \
+  -v /mnt/nvme_dataset:/mnt/dataset \
+  $image_name
+
+echo "Successfully created"
+
+# other options #
 
 # -v [host_path]:[mounting_point_in_the_container]
 # --gpus all / '"device=1"'
 # enable kvm
+# --device /dev/kvm \
+# --security-opt seccomp=unconfined \
+# --cap-add SYS_ADMIN \
